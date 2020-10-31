29fd7b484fa771fcef1a8806b5fd8a176874a3ff
lizhaopeng <1243893836@qq.com>
2020-10-31 22:55:18 +0800

    admin teacher student entity

---
+++ oes/src/main/java/com/itly/oes/entity/Admin.java
@@ -0,0 +1,86 @@
+package com.itly.oes.entity;
+
+import lombok.Data;
+
+@Data
+public class Admin {
+    private Integer adminId;
+
+    private String adminName;
+
+    private String sex;
+
+    private String tel;
+
+    private String email;
+
+    private String pwd;
+
+    private String cardId;
+
+    private String role;
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
